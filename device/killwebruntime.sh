3df243a47dcd1a62e45f76e4851e25f656d7cf31
Randall Barker <barkerr@lab126.com>
2012-07-10 16:54:58 -0700

    Fixed killwebruntime script.

--- device/killwebruntime.sh
+++ device/killwebruntime.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 busybox killall com.amazon.cloud9
 busybox killall com.amazon.cloud9
-busybox killall com.amazon.webruntime
-busybox killall com.amazon.webruntime
-busybox killall com.amazon.webruntime
+busybox killall webruntime
+busybox killall webruntime
+busybox killall webruntime
 
