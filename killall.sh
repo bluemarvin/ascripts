31282c00387ec1553c5c46b60f32ec755ba375cc
Randall E. Barker <simstorm@mac.com>
2012-07-23 12:13:33 -0700

    Updated build script to stip and compress webruntime binary before upload.

---
+++ killall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+adb shell pm disable com.amazon.webruntime
+adb shell /data/killwebruntime.sh
+adb shell /data/killwebruntime.sh
+adb shell /data/killwebruntime.sh
--- wrbuild.sh
+++ wrbuild.sh
@@ -3,6 +3,11 @@
 APK=false
 INSTALL=false
 GENERATE=false
+STAGE=false
+STAGING_DIR=$HOME/staging
+BINARY=webruntime
+SRC_DIR=$HOME/chromium/src/out/Release
+TARGET_DIR=/data/data/com.amazon.webruntime/files/webruntime
 
 failed()
 {
@@ -16,24 +21,29 @@ build_usage()
    echo "	-a            : create APK"
    echo "	-i            : install on device"
    echo "   -g            : generate gyp project"
+   echo "   -s            : stage binary"
 }
 
-
-while getopts aig ARG
+while getopts aigs ARG
 do
    case "$ARG" in
    a) APK=true;;
    i) INSTALL=true;;
    g) GENERATE=true;;
+   s) STAGE=true;;
    [?]) build_usage
         exit -1;;
    esac
 done
+
 cd $HOME/chromium/src
+
 source build/android/envsetup.sh
+
 if [ "true" = $GENERATE ] ; then
    android_gyp
 fi
+
 if [ "true" = $APK ] ; then
    make -j8 chrome_apk
 else
@@ -42,16 +52,29 @@ fi
 
 [ "$?" = "0" ] || failed "Build Failed"
 
+if [ ! -e "$STAGING_DIR" ] ; then
+   mkdir --parents $STAGING_DIR
+fi
+
+if [ "true" = $STAGE -o \( "true" = $INSTALL -a "false" = $APK \) ] ; then
+   $HOME/android-toolchain-eabi-64/arm-eabi/bin/strip $SRC_DIR/$BINARY -o $STAGING_DIR/$BINARY
+   [ "$?" = "0" ] || failed "Strip Failed"
+   gzip --best -f $STAGING_DIR/$BINARY
+   [ "$?" = "0" ] || failed "Compress Failed"
+fi
+
 if [ "true" = $INSTALL ] ; then
    adb shell /data/killwebruntime.sh
    if [ "true" = $APK ] ; then
       adb shell pm disable com.amazon.cloud9
       adb shell pm disable com.amazon.webruntime
-      adb install -r out/Release/WebRuntime-release.apk
+      adb install -r $SRC_DIR/WebRuntime-release.apk
       adb shell pm enable com.amazon.webruntime
       adb shell pm enable com.amazon.cloud9
    else
-      adb push out/Release/webruntime /data/data/com.amazon.webruntime/files/webruntime/.
-      adb push out/Release/webruntime.pak /data/data/com.amazon.webruntime/files/webruntime/.
+      adb push $STAGING_DIR/$BINARY.gz $TARGET_DIR/.
+      adb shell busybox gunzip -f $TARGET_DIR/$BINARY.gz
+      adb push $SRC_DIR/webruntime.pak $TARGET_DIR/.
    fi
+   [ "$?" = "0" ] || failed "Install Failed"
 fi
