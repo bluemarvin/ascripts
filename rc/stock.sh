4d4e2cb1bc1d791ff76e8691991567a733bfa0e3
Randall E. Barker <simstorm@mac.com>
2012-07-24 13:21:51 -0700

    Adding missing scripts path to the stock rc file.

--- rc/stock.sh
+++ rc/stock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 NUM_OF_JOBS=`cat /proc/cpuinfo | grep processor | wc -l`
+
 alias go="cd $HOME/chromium_stock/src"
 alias bld="time make -j$NUM_OF_JOBS BUILDTYPE=Release chrome"
 
@@ -8,10 +9,12 @@ export USE_CCACHE=1
 export PREBUILT_CCACHE_PATH=/usr/bin/ccache
 export CCACHE_DIR=$HOME/.ccache_stock
 
+MY_PATH=$HOME/scripts:$HOME/depot_tools
+
 if [ -z "$ORIG_PATH" ] ; then
-   export PATH=$HOME/depot_tools
+   export PATH=$MY_PATH
 else
-   export PATH=$ORIG_PATH:$HOME/depot_tools
+   export PATH=$ORIG_PATH:$MY_PATH
 fi
 
 export PS1='\[\e[1;31m\]STOCK\[\e[m\]:\w$(__git_ps1 " [%s]")\n>> '
