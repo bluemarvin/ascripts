a1c84514cf6e842e8fd8da210cde8fd888a5b32e
Randall Barker <rbarker@mozilla.com>
2018-07-24 15:09:19 -0700

    open browser when no command line

--- vrbopen.sh
+++ vrbopen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 url=$1
 
-if [ $url -z ] ; then
+if [ -z "$url" ] ; then
 exec adb shell am start -a android.intent.action.LAUNCH org.mozilla.vrbrowser/org.mozilla.vrbrowser.VRBrowserActivity
 else
 exec adb shell am start -a android.intent.action.VIEW -d "$url" org.mozilla.vrbrowser/org.mozilla.vrbrowser.VRBrowserActivity
